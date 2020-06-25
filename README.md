5addc19822b3d3ab94c016ddeaa22f33203312fb
sp21 <pshreyasgowda1997@gmail.com>
2020-06-25 12:33:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-hzdzkz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-hzdzkz)
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+var n: number[]=new Array(2);
+for(var intex of n)
+{
+  console.log(intex);
+}
